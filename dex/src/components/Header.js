b0c593e932c88dd4c1cf6c72178700d5a1ade038
fhaufiwunf <nguyentruonggiang123424k@gmail.com>
2024-06-07 15:54:31 +0700

    first commit

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import Logo from "../moralis-logo.svg";
+import Eth from "../eth.svg";
+import { Link } from 'react-router-dom';
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className='leftH'>
+        <img src={Logo} alt='logo'  className='logo'/>
+        <Link to="/" className="link">
+          <div className='headerItem'>swap</div>
+        </Link>
+        <Link to="/tokens" className="link">
+          <div className='headerItem'>tokens</div>
+        </Link>
+      
+      </div>
+      <div>sreach navbar</div>
+      <div className='rightH'>
+        <div className='headerItem'>
+          <img src={Eth} alt='eth' className='eth'/>
+          ethereum
+        </div>
+        <div className='connectButton'>
+          connect
+        </div>
+      </div>
+    </header>
   )
 }
 
